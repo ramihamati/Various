54352bc764399f9f5bc3cfa72ace0b3ba7fc7b3b
rami <hamati_rami2004@yahoo.com>
2024-04-29 22:52:19 +0100

    a

---
+++ JobWorkerAlpha/PipeReaderTcpClient1/Program.cs
@@ -0,0 +1,77 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.IO.Pipelines;
+using System.Net.Sockets;
+using System.Text;
+
+var client = new TcpNetworkConnection();
+var message = new Message { Content = "Hello, Server 1!", Timestamp = DateTime.Now };
+await client.SendMessageAsync("127.0.0.1", 8888, message);
+
+public class Message
+{
+    public string Content { get; set; }
+    public DateTime Timestamp { get; set; }
+}
+
+class TcpNetworkConnection
+{
+    // private readonly int _port;
+    //
+    // public TcpNetworkConnection(
+    //     int port)
+    // {
+    //     _port = port;
+    // }
+    //
+    //
+    
+    public async Task ReadAsync(NetworkStream stream)
+    {
+        var pipeReader = PipeReader.Create(stream);
+
+        while (true)
+        {
+            var result = await pipeReader.ReadAsync();
+            var buffer = result.Buffer;
+
+            foreach (var segment in buffer)
+            {
+                var messageBytes = segment.ToArray();
+                var messageString = Encoding.UTF8.GetString(messageBytes);
+                Console.WriteLine($"Received message from server: {messageString}");
+
+                // Here you can deserialize the messageBytes to a Message DTO if needed
+
+                // Respond to the client (echo back)
+                await stream.WriteAsync(messageBytes, 0, messageBytes.Length);
+            }
+
+            pipeReader.AdvanceTo(buffer.End);
+
+            // this should happen when client closes connection
+            // or when there is a response indicating that the no more
+            // data will be sent
+            if (result.IsCompleted)
+                break;
+        }
+    }
+
+    public async Task SendMessageAsync(string serverIp, int port, Message message)
+    {
+        var client = new TcpClient();
+        await client.ConnectAsync(serverIp, port);
+
+        var stream = client.GetStream();
+        
+
+        var pipeWriter = PipeWriter.Create(stream);
+
+        var messageString = $"{message.Content} - {message.Timestamp}";
+        var messageBytes = Encoding.UTF8.GetBytes(messageString);
+
+        await pipeWriter.WriteAsync(messageBytes);
+        await pipeWriter.FlushAsync();
+        await ReadAsync(stream);
+    }
+}
---
+++ JobWorkerAlpha/PipeWriterTcpServer/Program.cs
@@ -0,0 +1,57 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.IO.Pipelines;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+var server = new TcpServer();
+await server.StartServerAsync(8888);
+
+class TcpServer
+{
+    public async Task StartServerAsync(int port)
+    {
+        var listener = new TcpListener(IPAddress.Any, port);
+        listener.Start();
+
+        Console.WriteLine($"Server listening on port {port}");
+
+        while (true)
+        {
+            var client = await listener.AcceptTcpClientAsync();
+            _ = ProcessClientAsync(client);
+        }
+    }
+
+    private async Task ProcessClientAsync(TcpClient client)
+    {
+        var stream = client.GetStream();
+        var pipeReader = PipeReader.Create(stream);
+
+        while (true)
+        {
+            var result = await pipeReader.ReadAsync();
+            var buffer = result.Buffer;
+
+            foreach (var segment in buffer)
+            {
+                var messageBytes = segment.ToArray();
+                var messageString = Encoding.UTF8.GetString(messageBytes);
+                Console.WriteLine($"Received message from client: {messageString}");
+
+                // Here you can deserialize the messageBytes to a Message DTO if needed
+
+                // Respond to the client (echo back)
+                await stream.WriteAsync(messageBytes, 0, messageBytes.Length);
+            }
+
+            pipeReader.AdvanceTo(buffer.End);
+
+            if (result.IsCompleted)
+                break;
+        }
+
+        pipeReader.Complete();
+    }
+}
